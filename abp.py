b7f449d86758bb35fc0ee533d9cf7dfdc1eebfdb
Estevan Pequeno <estevan.pequeno@gmail.com>
2014-07-31 13:55:58 -0500

    install and start apache

--- abp.py
+++ abp.py
@@ -17,6 +17,7 @@ DOMAIN = raw_input("Enter domain [example.com]: ")
 if DOMAIN == '':
     DOMAIN = 'example.com'
 
+# Global constants
 APACHE_VHOST_DIR = '/etc/httpd/conf.d/'
 BASE_DIR = '/var/www/vhosts/'
 FULL_DIR = BASE_DIR + DOMAIN + '/'
@@ -24,15 +25,27 @@ STRUCTURE = ['admin', 'ftp', 'http', 'https', 'subdomains', 'logs']
 VHOST = ["<VirtualHost *:80>",
          "  ServerName " + DOMAIN,
          "  DocumentRoot " + FULL_DIR + "http",
+         "  ServerAlias ",
          "  ErrorLog " + FULL_DIR + "logs/error-log",
          "  CustomLog " + FULL_DIR + "logs/access-log common",
          "</VirtualHost>"]
+goodbye_msg = ["Configuration complete!",
+               "Please start httpd, chkconfig it on and open port 80!"]
 
 
 def test_exists():
-    if os.access("/var/www/vhosts/", os.F_OK):
+    '''Tests to make sure we aren't messing with a domain
+    that already has some directory set up'''
+
+    if os.access("/var/www/vhosts/" + DOMAIN, os.F_OK):
         print "Directory exists, exiting."
         sys.exit(1)
+
+
+def install_apache():
+    subprocess.call(["yum", "install", "httpd", "-y"])
+    subprocess.call(["chkconfig httpd on"], shell=True)
+    print "Installed Apache."
 
 
 def make_base_dir():
@@ -41,7 +54,9 @@ def make_base_dir():
 
 
 def make_structure():
-    global STRUCTURE
+    '''Add a set of default folders to the main domain directory'''
+
+    os.chdir(FULL_DIR)
     for directory in STRUCTURE:
         subprocess.call(["mkdir", directory])
     print "Made default directories (%s) in %s" % (STRUCTURE, FULL_DIR)
@@ -59,28 +74,43 @@ def change_permissions():
         sys.exit(1)
 
 
-def make_files():
+def make_conf_file():
+    '''Make a default configuration file for the virtualhost
+    using the settings made in the global constants section'''
+
     conf = open(APACHE_VHOST_DIR + DOMAIN + ".conf", 'w')
     for line in VHOST:
         conf.write(line + "\n")
     conf.close()
-    print "Created %s\n" % (conf.name)
+    print "Created %s" % (conf.name)
+
+
+def make_index_file():
+    '''Make a simple index.html file'''
+
     index = open(FULL_DIR + 'http/' + 'index.html', 'w')
     index.write("Hello World from " + DOMAIN)
     index.close()
+    print "Created %s" % (index.name)
 
 
-test_exists()
-make_base_dir()
-os.chdir(BASE_DIR + DOMAIN)
-make_structure()
-change_permissions()
-make_files()
+def start_apache():
+    subprocess.call(["service httpd start"], shell=True)
+    print "Starting Apache..."
 
-goodbye_msg = ["Configuration complete!",
-               "Please start httpd, chkconfig it on and run",
-               "the following command:\n",
-               "iptables -A INPUT -m conntrack --ctstate NEW -m tcp -p tcp --dport 80 -j ACCEPT"]
 
-for line in goodbye_msg:
-    print line
+def main():
+    test_exists()
+    install_apache()
+    make_base_dir()
+    make_structure()
+    change_permissions()
+    make_conf_file()
+    make_index_file()
+    start_apache()
+    print "\n"
+    for line in goodbye_msg:
+        print line
+
+if __name__ == '__main__':
+    main()
