b903d6e3c49c607abe5ae37b3f71f81d1ef6bd52
Estevan Pequeno <estevan.pequeno@gmail.com>
2014-08-04 19:03:19 -0500

    bash lol

---
+++ if-script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ $1 == 'eth0' ]; then
+	ip a | egrep "eth0$" | awk '{print $2}' | awk -F"/" '{print $1}'
+elif [ $1 == "eth0:0" ]; then
+	ip a | egrep "eth0:0$" | awk '{print $2}' | awk -F"/" '{print $1}'
+else
+	echo "invalid public IP"
+fi
