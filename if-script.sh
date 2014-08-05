d5bb107714c0865c678f906ef27446bf6c7849a0
Estevan Pequeno <estevan.pequeno@gmail.com>
2014-08-05 10:25:47 -0500

    make sure we know an error occoured

--- if-script.sh
+++ if-script.sh
@@ -5,5 +5,5 @@ if [ $1 == 'eth0' ]; then
 elif [ $1 == "eth0:0" ]; then
 	ip a | egrep "eth0:0$" | awk '{print $2}' | awk -F"/" '{print $1}'
 else
-	echo "invalid public IP"
+	echo "invalid public IP" >2 script.log
 fi
